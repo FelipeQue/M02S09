371dbde7e16579dbeb643f943c6a0f30792526b7
Felipe Quérette <felipe.querette@gmail.com>
2024-07-11 20:44:12 -0300

    update project files

--- src/main/java/br/com/fmt/M02S09/entities/Usuario.java
+++ src/main/java/br/com/fmt/M02S09/entities/Usuario.java
@@ -1,2 +1,55 @@
-package br.com.fmt.M02S09.entities;public class Usuario {
+package br.com.fmt.M02S09.entities;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        //return List.of();
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername(){
+        return username;
+        }
+
+
 }
--- src/main/java/br/com/fmt/M02S09/repositories/UsuarioRepository.java
+++ src/main/java/br/com/fmt/M02S09/repositories/UsuarioRepository.java
@@ -1,2 +1,15 @@
-package br.com.fmt.M02S09.repositories;public interface UsuarioRepository {
+package br.com.fmt.M02S09.repositories;
+
+import br.com.fmt.M02S09.entities.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByNomeUsuario(String nomeUsuario);
+
+
+
 }
